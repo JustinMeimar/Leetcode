fde7f4b782cbabe31915366ed4bed6a9ca25450d
Justin Meimar <j.meimar111@gmail.com>
2021-06-09 11:12:02 -0600

    c++ v added

---
+++ startTest.py
@@ -0,0 +1,16 @@
+phil = [0,1,2,3,4]
+
+i = 3
+
+n = 0
+while n < 12:
+    
+    print(phil[i%5])
+    i += 2
+    # if i == 5:
+    #     i = 0
+    # elif i == 6:
+    #     i = 1
+
+    n += 1
+
---
+++ test.py
@@ -0,0 +1,7 @@
+k = 68
+n = 2
+div = 0
+while k - n >=0:
+    k -= n
+    div += 1
+    print(div)
