91c3a4b23c1799f8332208f8b1e348a0ff741f49
Justin Meimar <j.meimar111@gmail.com>
2021-06-09 10:30:27 -0600

    first solution

---
+++ SumOfDigitsBaseK.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        print(n,k)
+        if n == 0:
+            return 0
+        i = 0
+        while k**i <= n/k:
+            i += 1
+            
+        print(i)
+
+        return 1 + self.sumBase(n-k**i,k)
+
+k = Solution()
+print(k.sumBase(68,2))
+print(k.sumBase(34,6))
